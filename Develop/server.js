1391f029285b34eb8408a442e671c71500cb1308
qosole <williamj99wei@gmail.com>
2022-07-22 14:09:26 -0500

    Add POST request for /api/diagnostics

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 const diagnostics = require('./db/diagnostics.json');
+const fs = require('fs');
 
 const PORT = process.env.port || 3001;
 
@@ -33,7 +34,30 @@ app.get('/api/diagnostics', (req, res) => {
   res.json(diagnostics);
 })
 
+// POST route for /api/diagnostics
+app.post('/api/diagnostics', (req, res) => {
+  const {time, error_id, errors} = req.body;
 
+  // Verfiying that the request is valid
+  if (time && error_id && errors) {
+    const newDiagnostic = {
+      time,
+      error_id,
+      errors
+    };
+
+    fs.readFile('./db/diagnostics.json', 'utf-8', (err, data) => {
+      let existingData = JSON.parse(data);
+      existingData.push(newDiagnostic);
+      fs.writeFile('./db/diagnostics.json', JSON.stringify(existingData), (err) => {
+        err ? console.log(err) : console.log('Data successfully written!');
+        res.json(existingData);
+      })
+    })
+  } else {
+    res.json('error');
+  }
+})
 
 // GET Route for wildcard routes
 app.get('*', (req, res) => {
