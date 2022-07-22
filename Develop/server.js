7ad8272a2bcbdfc4e9a45ad326dd60717bfe6cdc
qosole <williamj99wei@gmail.com>
2022-07-22 13:37:21 -0500

    Add wildcard route and 404 page

--- Develop/server.js
+++ Develop/server.js
@@ -27,6 +27,13 @@ app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
 );
 
+// Get Route for /api/diagnostics
+
+// GET Route for wildcard routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/404.html'))
+});
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
